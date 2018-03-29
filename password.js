8fb3ad1d19b33d922a2ef5939bb96294cb8c28fd
Derrick <dmateja724@gmail.com>
2018-03-29 10:55:29 -0500

    added comments

--- password.js
+++ password.js
@@ -1,39 +1,45 @@
 "use strict";
-var $ = function(id) { return document.getElementById(id); };
+var $ = function(id) { 
+    return document.getElementById(id); 
+};
 
 var getRandomNumber = function(max) {
     var random;
     if (!isNaN(max)) {
-        random = Math.random(); //value >= 0.0 and < 1.0
-        random = Math.floor(random * max); //value is an integer between 0 and max - 1
-        random = random + 1; //value is an integer between 1 and max
+        random = Math.random();               //value >= 0.0 and < 1.0
+        random = Math.floor(random * max);   //value is an integer between 0 and max - 1
+        random = random + 1;                //value is an integer between 1 and max
     }
     return random;
 };
 
+
+// gives functionality to "get password" button 
 var generatePassword = function() {
-    $("password").value = ""; // clear previous entry
-    var passChar = "";
-    var pass = "";
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
+    $("password").value = "";       // clear previous entry
+    var passChar = "";             // single random character variable
+    var pass = "";                // generated password container
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@"; // pool of characters to be chosen 
     var i = 0;
     
-    
+    // validation for a numeric entry
     if (isNaN($("num").value)) {
         alert("Please enter a valid number.");
         $("num").value = "";
         $("num").focus();
     } 
     
+    // loop that grabs random characters and builds the password
     while (i < $("num").value) {
         i++;
         var random = getRandomNumber(chars.length);
         passChar = chars[random];
         pass += passChar;
     }
-    $("password").value = pass;
+    $("password").value = pass;     // displays password to the user
 };
 
+// gives the "clear" button functionality 
 var clearFields = function() {
     $("num").value = "";
     $("password").value = "";
